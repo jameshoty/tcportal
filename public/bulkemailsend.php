9c58a5ba957d3620fcd99f6f968a8f196c9217b5
James HO <jameshoty@me.com>
2019-03-05 21:58:16 +0800

    Multiple address added

--- public/bulkemailsend.php
+++ public/bulkemailsend.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //Recipients
         $mail->setFrom('jameshoty@me.com', 'James');
         // Add each email into the $mail object      
-        $email_recipients = explode(',',$emails);
+        $email_recipients = explode(',',$emails,-1);
         foreach ($email_recipients as $recipient) {
             $mail->addAddress($recipient);
         }
